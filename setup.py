a3d28bed8ad26b03e4144bbe0fdf39156a2940bc
PolBaladas <polbaladasluna@gmail.com>
2017-03-22 19:17:30 +0100

    Download_URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Pol Baladas',
   author_email = 'polbaladasluna@gmail.com',
   url = 'https://github.com/PolBaladas/torsimany',
-  download_url = 'https://github.com/PolBaladas/torsimany/tarball/0.17',
+  download_url = 'https://github.com/PolBaladas/torsimany/tarball/1.0',
   keywords = ['markdown', 'json', 'convert'], 
   entry_points={
     'console_scripts': [
